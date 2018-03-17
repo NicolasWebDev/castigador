bcdece6bf849a0e32945810a94db5fbb8ca76e6e
Sathors <sathors@free.fr>
2018-03-17 14:08:03 -0500

    Add tests.

--- spec/Episode.spec.js
+++ spec/Episode.spec.js
@@ -1,8 +1,30 @@
 import Episode from '../src/Episode'
+import helpers from '../src/helpers'
 
 describe('Episode', () => {
-  it('nothing', () => {
-    const nothing = new Episode()
-    expect(true).toBe(true)
+  let episode
+  let logOutput
+
+  beforeEach(() => {
+    episode = new Episode('Scrum secrets', 'http://dummy.org')
+  })
+
+  describe('download', () => {
+    it('calls helpers.downloadAudioOnPage with the url', async () => {
+      jest.spyOn(helpers, 'downloadAudioOnPage').mockReturnValue()
+
+      await episode.download()
+
+      expect(helpers.downloadAudioOnPage)
+        .toHaveBeenCalledWith('http://dummy.org')
+    })
+
+    it('catches the error if there is one', async () => {
+      jest.spyOn(helpers, 'downloadAudioOnPage').mockImplementation(() => {
+        throw Error
+      })
+
+      await episode.download()
+    })
   })
 })
